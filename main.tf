49cc58ccd9228252adb6c2d2d187b084cb5cbf7a
april5mj <may5256@126.com>
2026-03-24 11:13:36 +0800

    test_simple

--- main.tf
+++ main.tf
@@ -1,28 +1,30 @@
-
-variable "cidr_blocks" {
-    description = "vpc , subnet cider block"
-    type = list(object({
-      cidr_block = string
-      name = string
-    }))   #list type
-}         
-
-# define variables
-
-
 resource "aws_vpc" "vpc_dev" {  
-    cidr_block = var.cidr_blocks[0].cidr_block  
+    cidr_block = "10.0.0.0/16"
     tags = {
-        Name: var.cidr_blocks[0].name
+        Name:"VPC_dev"
         owner:"spsapr"
-    }
+    }                  #加了名字, owner tag
 }
 
 resource "aws_subnet" "subnet_01" {
     vpc_id = aws_vpc.vpc_dev.id  #引用还没生成的vpc的id
-    cidr_block =  var.cidr_blocks[1].cidr_block    #use variable
+    cidr_block = "10.0.10.0/24"
     availability_zone = "ap-northeast-3a"
         tags = {
-        Name:var.cidr_blocks[1].name
+        Name:"subnet_01_dev"
+    }
+}
+
+data "aws_vpc" "exsiting_vpc" {
+    default = false
+    cidr_block = "192.168.0.0/16"
+}
+
+resource "aws_subnet" "subnet_02" {
+    vpc_id = data.aws_vpc.exsiting_vpc.id  #引用query到的vpc id
+    cidr_block = "192.168.5.0/24"
+    availability_zone = "ap-northeast-3a"
+        tags = {
+        Name:"subnet_02_dev"
     }
 }
