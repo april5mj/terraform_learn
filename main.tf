48cc8e18ab3234279fd9fee145ff8d89641cc45b
april5mj <may5256@126.com>
2026-03-27 09:16:45 +0800

    use_remote_module

--- main.tf
+++ main.tf
@@ -2,32 +2,32 @@ provider "aws" {
   # region 从环境变量 AWS_REGION 读取
 }
 
-# # 动态查询最新的 Amazon Linux 2023 AMI
-# data "aws_ami" "al2023" {
-#   most_recent = true
-#   owners      = ["amazon"]
+# 动态查询最新的 Amazon Linux 2023 AMI
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["amazon"]
 
-#   filter {
-#     name   = "name"
-#     values = ["al2023-ami-2023.*-x86_64"]
-#   }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
 
-#   filter {
-#     name   = "state"
-#     values = ["available"]
-#   }
-# }
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
 
-# module "ec2_example" {
-#   source = "./modules/ec2"
+module "ec2_example" {
+  source = "app.terraform.io/Coach-learn/ec2/aws"
+  #"./modules/ec2"
+  name        = "april-instance"
+  ami_id      = data.aws_ami.al2023.id  # 自动获取最新 AL2023
+  subnet_id   = "subnet-0fcb87ab84b39a2ce"
+  cost_center = "team-backend"          # 必填，不传会报错
 
-#   name        = "my-instance"
-#   ami_id      = data.aws_ami.al2023.id  # 自动获取最新 AL2023
-#   subnet_id   = "subnet-0fcb87ab84b39a2ce"
-#   cost_center = "team-backend"          # 必填，不传会报错
-
-#   # extra_tags 可选
-#   extra_tags = {
-#     Environment = "dev"
-#   }
-# }
+  # extra_tags 可选
+  extra_tags = {
+    Environment = "dev"
+  }
+}
